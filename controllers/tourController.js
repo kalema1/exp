2d75ca9f028432e65bfaa3674d23adff88725300
kalema1 <kalemar8@gmail.com>
2025-01-15 11:36:20 +0300

    create aliseTopFiveTours middleware
    - added logic to the middleware function to filteer out fields befor calling the getAllTours.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,6 +18,15 @@ const Tour = require("./../models/tourModel");
   next();
 };
  */
+
+exports.aliasTopFiveTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort("-ratingsAverage,price");
+  req.query.fields = "name,price,ratingsAverage,difficulty";
+
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,16 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopFiveTours,
 } = require("./../controllers/tourController");
 
 const router = express.Router();
 
 //param middleware
 //router.param("id", checkID);
+
+// alias route - we use middleware function first b4 we call the get all tours function
+router.route("/top-5-cheap").get(aliasTopFiveTours, getAllTours);
 
 router.route("/").get(getAllTours).post(createTour);
 
