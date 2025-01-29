d8578dd7c8bbfb41d3e8241d6ad587c33a6ee33f
kalema1 <kalemar8@gmail.com>
2025-01-29 14:23:28 +0300

    feat: imppement rate limiting
    - added logic to the app.js file. to limit requests from a single
      IP.This prevents attacks to the application.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,6 +10,13 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 //middleware
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour",
+});
+app.use("/api", limiter);
+
 app.use(morgan("dev"));
 app.use(express.json());
 
