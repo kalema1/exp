028e85b471a823461bdc6da10a8cd7d4e0279207
kalema1 <kalemar8@gmail.com>
2025-01-29 11:57:00 +0300

    feat: update password
    - created the updatePassword handler. to contain logic to update user
      password of a logged in user.
    - added logic  to the function.
    - added a route to update the password.

--- controllers/authController.js
+++ controllers/authController.js
@@ -181,3 +181,26 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  // 1) get user from the collection
+  const user = await User.findById(req.user.id).select("+password");
+
+  // 2) check if POSTed user password is correct
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError("Your current password is wrong", 401));
+  }
+
+  // 3) if passwrd correct, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  // 4) Log user in, send JWT
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,8 @@ const {
   login,
   forgotPassword,
   resetPassword,
+  protect,
+  updatePassword,
 } = require("../controllers/authController");
 
 const router = express.Router();
@@ -20,6 +22,8 @@ router.post("/login", login);
 
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
+
+router.patch("/updateMyPassword", protect, updatePassword);
 
 router.route("/").get(getAllUsers).post(createUser);
 
